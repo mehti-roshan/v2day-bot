013393de384a0d5215ce7843897baf94f1849e73
mehti <mehti6772@gmail.com>
2025-06-10 23:39:42 +0330

    added CARD_OWNER_NAME to environment and added additional description text

--- src/env.ts
+++ src/env.ts
@@ -4,5 +4,6 @@ export default z.object({
   BOT_TOKEN: z.string(),
   REDIS_HOST: z.string(),
   CARD_NUMBER: z.string(),
-  BANK_NAME: z.string()
+  BANK_NAME: z.string(),
+  CARD_OWNER_NAME: z.string(),
 }).parse(process.env);
